34a38db2277581fe803e6b81154bb20d32865a6c
Olzhas <o.arystanov@gmail.com>
2018-05-31 19:25:01 +0600

    Added ALLOWED_HOSTS to production settings

--- update/settings/production.py
+++ update/settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'update-architects.com',
+                 '139.162.175.130']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '(b56aueko1(^$72ce5%7xh5feejh&64&h_tbh%1476vv1ubjpm'
+
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
