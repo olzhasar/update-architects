a5604124aaaa334358dd542cd7799448d4f689f4
Olzhas <o.arystanov@gmail.com>
2019-06-14 03:27:31 +0600

    Added robots.txt

--- update/urls.py
+++ update/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -17,6 +18,8 @@ urlpatterns = [
 
     url(r'^search/$', search_views.search, name='search'),
     url(r'^sitemap\.xml$', sitemap),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',
+                                               content_type='text/plain')),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
