65f79d190230b9de1f43dbac048c1c1b88e175c6
Olzhas <o.arystanov@gmail.com>
2019-07-22 03:38:06 +0000

    Cleaned DATABASES setting

--- update/settings/production.py
+++ update/settings/production.py
@@ -32,12 +32,12 @@ WAGTAIL_ENABLE_UPDATE_CHECK = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', ''),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
