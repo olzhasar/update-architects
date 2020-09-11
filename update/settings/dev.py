04e19999ef2cc933d65f8756121e51af3ea9f267
olzhasar <o.arystanov@gmail.com>
2020-09-11 23:55:02 +0600

    Updated settings

--- update/settings/dev.py
+++ update/settings/dev.py
@@ -2,31 +2,15 @@ from .base import *
 
 DEBUG = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "update",
+        "USER": "update",
+        "PASSWORD": "update",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'update',
-        'USER': 'update',
-        'PASSWORD': 'update',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
--- update/settings/production.py
+++ update/settings/production.py
@@ -1,60 +1,37 @@
-import os
 from .base import *
 
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'localhost', '127.0.0.1',
-    'up2date.kz',
-    'www.up2date.kz',
+    "localhost",
+    "127.0.0.1",
+    "up2date.kz",
+    "www.up2date.kz",
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-STATIC_ROOT = '/var/www/static'
-
-MEDIA_ROOT = '/var/www/media'
-
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = False
-#COMPRESS_CSS_FILTERS = ['compressor.filters.cssmin.CSSCompressorFilter']
-#COMPRESS_JS_FILTERS = ['compressor.filters.jsmin.SlimItFilter']
+# COMPRESS_CSS_FILTERS = ['compressor.filters.cssmin.CSSCompressorFilter']
+# COMPRESS_JS_FILTERS = ['compressor.filters.jsmin.SlimItFilter']
 
 WAGTAILIMAGES_JPEG_QUALITY = 80
 
 WAGTAIL_ENABLE_UPDATE_CHECK = False
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
-}
-
 CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'localhost:6379',
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "localhost:6379",
     }
 }
 
-EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST = "smtp.yandex.ru"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
-try:
-    from .local import *
-except ImportError:
-    pass
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
