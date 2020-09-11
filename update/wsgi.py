6f4539a1dca9158d116208000c6e17a40aec5449
olzhasar <o.arystanov@gmail.com>
2020-09-11 23:57:13 +0600

    Added python-dotenv

--- update/wsgi.py
+++ update/wsgi.py
@@ -1,15 +1,9 @@
-"""
-WSGI config for update project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+load_dotenv()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "update.settings.dev")
 
