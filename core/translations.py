12229de790188dce8ff6dd39cc48bb3da3101873
olzhasar <o.arystanov@gmail.com>
2020-09-12 14:41:31 +0600

    Added translations descriptor

---
+++ core/translations.py
@@ -0,0 +1,23 @@
+from django.utils.translation import get_language
+
+
+class TranslationDescriptor:
+    def __init__(self, instance, locale):
+        prefix_mapping = {"ru-ru": "ru", "en-us": "en"}
+        self.instance = instance
+        self.prefix = prefix_mapping.get(locale, "ru")
+
+    def __getattr__(self, field_name):
+        translated_field = f"{self.prefix}_{field_name}"
+        if hasattr(self.instance, translated_field):
+            return getattr(self.instance, translated_field)
+        return getattr(self.instance, field_name)
+
+    def __str__(self):
+        return str(self.instance)
+
+
+class TranslationProxy:
+    def __get__(self, instance, owner):
+        locale = get_language()
+        return TranslationDescriptor(instance, locale)
