68eacd08be8754b3f6cc89c16c27ff8694ba1824
olzhasar <o.arystanov@gmail.com>
2020-09-15 16:04:10 +0600

    Added set_language view to urls

--- update/urls.py
+++ update/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.views.i18n import set_language
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
@@ -15,6 +16,7 @@ urlpatterns = [
     url(r"^admin/", include(wagtailadmin_urls)),
     url(r"^documents/", include(wagtaildocs_urls)),
     url(r"^sitemap\.xml$", sitemap),
+    url(r"^set_lang/", set_language, name="set_language"),
     url(
         r"^robots\.txt$",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
