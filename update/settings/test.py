ab09473f4d0ce7e2cfb61689c202a736307d2501
olzhasar <o.arystanov@gmail.com>
2020-09-26 21:04:57 +0600

    Added pytest.ini, test settings

---
+++ update/settings/test.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "update_test",
+        "USER": "update",
+        "PASSWORD": "update",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
