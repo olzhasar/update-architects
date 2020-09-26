b640ba2cd9a8271d29a1ae1ff6d73ff3d00c762c
olzhasar <o.arystanov@gmail.com>
2020-09-26 21:08:46 +0600

    Cleaned test settings

--- update/settings/test.py
+++ update/settings/test.py
@@ -1,16 +1,1 @@
-from .base import *
-
-DEBUG = True
-
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "update_test",
-        "USER": "update",
-        "PASSWORD": "update",
-        "HOST": "localhost",
-        "PORT": "",
-    }
-}
+from .dev import *
